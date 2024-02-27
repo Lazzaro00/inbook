504c3fde42f91a210c1d504001243e0b0e41184c
Lazzaro <lazzaro.carlucci@libero.it>
2024-02-27 09:57:28 +0100

    First commit

---
+++ src/main/java/it/contrader/inbook/InbookApplication.java
@@ -0,0 +1,13 @@
+package it.contrader.inbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/contrader/inbook/InbookApplicationTests.java
@@ -0,0 +1,13 @@
+package it.contrader.inbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
