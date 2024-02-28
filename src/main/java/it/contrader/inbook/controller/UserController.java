37a8821e641b9c15933857ecfac473ab64f48708
Pasquale <pasqualepiobasile@gmail.com>
2024-02-28 12:30:44 +0100

    Lato funzionante login

--- src/main/java/it/contrader/inbook/controller/UserController.java
+++ src/main/java/it/contrader/inbook/controller/UserController.java
@@ -1,17 +1,27 @@
 package it.contrader.inbook.controller;
 
+import it.contrader.inbook.dto.LoggedDTO;
+import it.contrader.inbook.dto.LoginDTO;
 import it.contrader.inbook.dto.UserDTO;
+import it.contrader.inbook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("http://localhost:4200")
 public class UserController extends AbstractController<UserDTO>{
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoggedDTO> login(@RequestBody @Validated LoginDTO loginDTO){
+        return new ResponseEntity<LoggedDTO>(userService.login(loginDTO), HttpStatus.OK);
+    }
 
 
 }
