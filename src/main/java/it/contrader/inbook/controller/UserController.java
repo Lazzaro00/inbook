ee7b7c5d38cfc77d89452bee3f36eb433ce4dc61
Pasquale <pasqualepiobasile@gmail.com>
2024-02-28 11:49:04 +0100

    branch Login

--- src/main/java/it/contrader/inbook/controller/UserController.java
+++ src/main/java/it/contrader/inbook/controller/UserController.java
@@ -1,7 +1,9 @@
 package it.contrader.inbook.controller;
 
 import it.contrader.inbook.dto.UserDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @CrossOrigin("http://localhost:4200")
 public class UserController extends AbstractController<UserDTO>{
+
+    @PostMapping("/login")
+    public ResponseEntity<>
 
 
 }
