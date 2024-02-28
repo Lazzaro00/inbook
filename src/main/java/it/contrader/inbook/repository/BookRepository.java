21a14a99ca8f8e2855b780a54926d5a436898bce
GianlucaPatruno <bof__9520>
2024-02-28 11:58:38 +0100

    Aggiunta filtri ricerca

--- src/main/java/it/contrader/inbook/repository/BookRepository.java
+++ src/main/java/it/contrader/inbook/repository/BookRepository.java
@@ -3,5 +3,12 @@ package it.contrader.inbook.repository;
 import it.contrader.inbook.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Long> {
+    List<Book> findByName(String name);
+
+    List<Book> findByCategory(String category);
+
+    List<Book> findByPriceGreaterThan(double price);
 }
