8a30751e24913d47f59aa40a69484ad902972df9
GianlucaPatruno <bof__9520>
2024-02-28 14:45:33 +0100

    Aggiunte funzionalità per le ricerche

--- src/main/java/it/contrader/inbook/service/BuyService.java
+++ src/main/java/it/contrader/inbook/service/BuyService.java
@@ -1,8 +1,22 @@
 package it.contrader.inbook.service;
 
+import it.contrader.inbook.converter.BuyConverter;
 import it.contrader.inbook.dto.BuyDTO;
 import it.contrader.inbook.model.Buy;
+import it.contrader.inbook.repository.BuyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class BuyService extends AbstractService<Buy, BuyDTO>{
 
+    @Autowired
+    BuyConverter converter;
+
+    @Autowired
+    BuyRepository repository;
+
+    public List<BuyDTO> getByUser_IdAndBook_Id(long user_Id, long book_Id){
+        return converter.toListDTO(repository.findByUser_IdAndBook_Id(user_Id, book_Id));
+    }
 }
--- src/main/java/it/contrader/inbook/service/LibraryService.java
+++ src/main/java/it/contrader/inbook/service/LibraryService.java
@@ -1,7 +1,22 @@
 package it.contrader.inbook.service;
 
+import it.contrader.inbook.converter.LibraryConverter;
 import it.contrader.inbook.dto.LibraryDTO;
 import it.contrader.inbook.model.Library;
+import it.contrader.inbook.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class LibraryService extends AbstractService<Library, LibraryDTO>{
+
+    @Autowired
+    LibraryConverter converter;
+
+    @Autowired
+    LibraryRepository repository;
+
+    public List<LibraryDTO> getByAdmin_Id(long adminId){
+        return converter.toListDTO(repository.findByAdmins_Id(adminId));
+    }
 }
