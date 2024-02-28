44d0765331048b4adf1fef96cff439c38eee24b2
GianlucaPatruno <bof__9520>
2024-02-28 12:48:04 +0100

    Aggiunte funzionalità per le ricerche

--- src/main/java/it/contrader/inbook/service/AnagraphicService.java
+++ src/main/java/it/contrader/inbook/service/AnagraphicService.java
@@ -1,7 +1,24 @@
 package it.contrader.inbook.service;
 
+import it.contrader.inbook.converter.AnagraphicConverter;
 import it.contrader.inbook.dto.AnagraphicDTO;
 import it.contrader.inbook.model.Anagraphic;
+import it.contrader.inbook.repository.AnagraphicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class AnagraphicService extends AbstractService<Anagraphic, AnagraphicDTO> {
+
+    @Autowired
+    AnagraphicConverter converter;
+
+    @Autowired
+    AnagraphicRepository repository;
+
+    public AnagraphicDTO getByUserId(long userId){
+        Anagraphic an = repository.findByUser_Id(userId).orElse(null);
+        return an != null ? converter.toDTO(an) : null;
+    }
+
+
 }
--- src/main/java/it/contrader/inbook/service/BookService.java
+++ src/main/java/it/contrader/inbook/service/BookService.java
@@ -1,7 +1,23 @@
 package it.contrader.inbook.service;
 
+import it.contrader.inbook.controller.BookController;
+import it.contrader.inbook.converter.BookConverter;
 import it.contrader.inbook.dto.BookDTO;
 import it.contrader.inbook.model.Book;
+import it.contrader.inbook.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class BookService extends AbstractService<Book, BookDTO> {
+
+    @Autowired
+    BookRepository repository;
+
+    @Autowired
+    BookConverter converter;
+
+    public List<BookDTO> getByName(String name){
+        return converter.toListDTO(repository.findByName(name));
+    }
 }
